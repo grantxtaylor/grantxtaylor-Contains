430ac6a23ac96f91a5fc7e64e3c7ecf32d00fc19
grantxtaylor <xgrtaylor@gmail.com>
2023-02-27 16:13:02 +0000

    ,

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -4,7 +4,7 @@ public class Contains {
 
     /**
      * This method should search an array to see if that array contains some value.
-     * You will need to use a for loop to check every value of the array to see if it matches the target value..
+     * You willl need to use a for loop to check every value of the array to see if it matches the target value..
      *
      * @param arr array to be manipulated.
      * @param target the value that the method should be searching for.
